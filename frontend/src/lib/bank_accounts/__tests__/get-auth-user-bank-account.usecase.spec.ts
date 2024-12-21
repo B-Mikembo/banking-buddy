83174da85c9d04a5f072b5bad5ac6232de09a0e9
B-Mikembo <mikembo.brice.pro@gmail.com>
2024-12-21 10:36:00 +0100

    structure d'un test unitaire

--- frontend/src/lib/bank_accounts/__tests__/get-auth-user-bank-account.usecase.spec.ts
+++ frontend/src/lib/bank_accounts/__tests__/get-auth-user-bank-account.usecase.spec.ts
@@ -1,7 +1,66 @@
 import {describe, it} from "vitest"
 
-describe("test suite", () => {
-    it("it works", ({expect}) => {
-        expect(true).toBe(true);
+describe("Feature: Retrieving authenticated user's bank accounts", () => {
+    it("Example: Bob is authenticated and can see his bank accounts", ({expect}) => {
+        givenAuthenticatedUserIs("Bob");
+        givenExistingBankAccounts({
+            user: "Bob",
+            bankAccounts: [
+                {
+                    name: "SG",
+                    transactions: [
+                        {
+                            name: "Spotify",
+                            amount: -15.00,
+                            status: "En cours",
+                            date: new Date("2024-12-01T10:00:00.000Z"),
+                            category: "Abonnement"
+                        }
+                    ]
+                },
+                {
+                    name: "BoursoBank",
+                    transactions: [
+                        {
+                            name: "Disney+",
+                            amount: -120.00,
+                            status: "Succès",
+                            date: new Date("2024-01-05T10:00:00.000Z"),
+                            category: "Abonnement"
+                        }
+                    ]
+                }
+            ]
+        });
+        await whenRetrievingAuthenticatedUserBankAccounts();
+
+        thenTheReceivedBankAccountShouldBe({
+            bankAccounts: [
+                {
+                    name: "SG",
+                    transactions: [
+                        {
+                            name: "Spotify",
+                            amount: -15.00,
+                            status: "En cours",
+                            date: new Date("2024-12-01T10:00:00.000Z"),
+                            category: "Abonnement"
+                        }
+                    ]
+                },
+                {
+                    name: "BoursoBank",
+                    transactions: [
+                        {
+                            name: "Disney+",
+                            amount: -120.00,
+                            status: "Succès",
+                            date: new Date("2024-01-05T10:00:00.000Z"),
+                            category: "Abonnement"
+                        }
+                    ]
+                }
+            ]
+        })
     });
 });

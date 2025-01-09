2eeb5460d8bc581f1fd2f5f4ce74877b43821fbc
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-01-09 18:36:26 +0100

    feat: afficher le champs email

---
+++ src/components/dsfr/EmailInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="fr-input-group">
+    <label class="fr-label" :class="classLabel" :for="name">
+      {{ label }}
+      <span class="text--normal fr-hint-text"
+        >Format attendu : nom@domain.fr</span
+      >
+    </label>
+    <input
+      :id="name"
+      :name="name"
+      class="fr-input"
+      autocomplete="on"
+      type="email"
+      :disabled="disable"
+      required
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  label: string;
+  classLabel?: string;
+  disable?: boolean;
+}>();
+</script>
---
+++ src/components/pages/Page404.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Page non trouvée</h1>
+  <p>La page que vous cherchez est introuvable. Excusez-nous pour la gène occasionnée.</p>
+</template>
+<script setup lang="ts"></script>

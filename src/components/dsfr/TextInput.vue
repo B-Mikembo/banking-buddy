7a2f5ed50e72d5ece424040bcf5fe121d27caff4
Brice Mikembo <bmikembo@etud.u-pem.fr>
2025-01-10 19:47:50 +0100

    fix: resolution d'un soucis dans le composant TextInput.vue

--- src/components/dsfr/TextInput.vue
+++ src/components/dsfr/TextInput.vue
@@ -4,7 +4,7 @@
             {{ label }}
             <span v-if="description" class="fr-hint-text">{{ description }}</span>
         </label>
-        <input class="fr-input" :id="name" :name="name" @input:"updateValue" :required="required" :maxlength="maxlength"
+        <input class="fr-input" :id="name" :name="name" :required="required" :maxlength="maxlength"
             :autofocus="autofocus" />
         <p v-if="error && error.show" id="text-input-error-desc-error" class="fr-error-text">{{ error.message }}</p>
     </div>

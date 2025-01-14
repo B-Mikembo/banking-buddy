08311df44801273a82f5c88faefe07d8f8d750a6
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-01-14 22:11:17 +0100

    feat: mise à jour du header

--- src/components/AccountCreationForm.vue
+++ src/components/AccountCreationForm.vue
@@ -1,23 +1,21 @@
 <template>
-    <main class="flex min-h-screen w-full justify-between font-inter">
-        <section class="flex-center size-full max-sm:px-6 bg-zinc-300">
-            <section>
-                <header class="flex flex-col gap-5 md:gap-8">
-                    <h1 class="text-26 font-bold text-black-1">Noodle Bank</h1>
-                    <div class="flex flex-col gap-1 md:gap-3">
-                        <h1 class="text-24 lg:text-36 font-semibold text-gray-900">
-                            S'inscrire
-                            <p class="text-16 font-normal text-gray-600">Entrez vos informations</p>
-                        </h1>
-                    </div>
-                </header>
-            </section>
-        </section>
-    </main>
+  <section class="auth-form">
+    <header class="flex flex-col gap-5 md:gap-8">
+      <a href="/" class="cursor-pointer flex items-center gap-1">
+        <img src="/vite.svg" width="34" height="34" alt="Vite logo" />
+        <h1 class="text-26 font-ibm-plex-serif font-bold text-black-1">Noodle Bank</h1>
+      </a>
+      <div class="flex flex-col gap-1 md:gap-3">
+        <h1 class="text-24 lg:text-36 font-semibold text-gray-900">
+          S'inscrire
+          <p class="text-16 font-normal text-gray-600">Entrez vos informations</p>
+        </h1>
+      </div>
+    </header>
+  </section>
 </template>
-
 <script setup lang="ts">
-import EmailInput from '@/components/dsfr/EmailInput.vue';
-import PasswordInput from '@/components/custom/PasswordInput.vue';
-import TextInput from '@/components/dsfr/TextInput.vue';
+  import EmailInput from '@/components/dsfr/EmailInput.vue';
+  import PasswordInput from '@/components/custom/PasswordInput.vue';
+  import TextInput from '@/components/TextInput.vue';
 </script>
--- src/pages/AccountCreationPage.vue
+++ src/pages/AccountCreationPage.vue
@@ -1,7 +1,14 @@
 <template>
-  <section class="flex-center size-full max-sm:px-6">
-    <AccountCreationForm />
-  </section>
+  <main class="flex min-h-screen w-full justify-between font-inter">
+    <section class="flex-center size-full max-sm:px-6">
+      <AccountCreationForm />
+    </section>
+    <div class="auth-asset">
+      <div>
+        <img src="/troll.jpg" alt="Auth image" width="500" height="500" class="rounded-l-xl object-contain" />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">

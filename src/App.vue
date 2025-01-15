336e56dbfd67453cb7c8f099ab5c6bca29c5e1d0
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-01-15 22:16:32 +0100

    tech: init nuxt 3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="page-container">
     <main id="contenu" class="background--main">
-        <Flow />
-  <RouterView v-slot="{ Component }">
+      <Flow />
+      <RouterView v-slot="{ Component }">
         <Transition name="slide">
-          <component :is="Component"/>
+          <component :is="Component" />
         </Transition>
       </RouterView>
     </main>
@@ -12,17 +12,12 @@
 </template>
 
 <script setup lang="ts">
-import Flow from './components/dashboard/Flow.vue';
-import { RouteLocationNormalized, NavigationGuardNext } from "vue-router";
-import router from "@/router";
+  import Flow from './components/dashboard/Flow.vue';
+  import { RouteLocationNormalized, NavigationGuardNext } from 'vue-router';
+  import router from '@/router';
 
-const appName = "- Noodle Bank";
-router.beforeEach(
-  (
-    to: RouteLocationNormalized,
-    from: RouteLocationNormalized,
-    next: NavigationGuardNext
-  ) => {
+  const appName = '- Noodle Bank';
+  router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
     const { title, isPublic } = to.meta;
     if (title) {
       document.title = `${title as string} ${appName}`;
@@ -30,32 +25,35 @@ router.beforeEach(
     if (isPublic) {
       next();
     } else {
-      next({ name: "authentication" });
-      sessionStorage.setItem("requestedRoute", to.fullPath);
+      next({ name: 'authentication' });
+      sessionStorage.setItem('requestedRoute', to.fullPath);
     }
-  }
-);
+  });
 </script>
 
 <style scoped>
-.page-container {
-  display: grid;
-  grid-template-rows: auto 1fr auto;
-  min-height: 100vh;
-}
+  .page-container {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+  }
   .slide-enter-active {
-  transition: transform 0.7s ease, opacity 0.7s ease;
-}
-.slide-enter-from {
-  transform: translateY(+100%);
-  opacity: 0;
-}
-/* Slide-out to the top */
-.slide-leave-active {
-  transition: transform 0.2s ease, opacity 0.2s ease;
-}
-.slide-leave-to {
-  transform: translateY(-100%);
-  opacity: 0;
-}
+    transition:
+      transform 0.7s ease,
+      opacity 0.7s ease;
+  }
+  .slide-enter-from {
+    transform: translateY(+100%);
+    opacity: 0;
+  }
+  /* Slide-out to the top */
+  .slide-leave-active {
+    transition:
+      transform 0.2s ease,
+      opacity 0.2s ease;
+  }
+  .slide-leave-to {
+    transform: translateY(-100%);
+    opacity: 0;
+  }
 </style>

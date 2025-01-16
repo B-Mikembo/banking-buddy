225e4a6927fc31a6b686d99395bf7e3ff670ae28
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-01-16 12:50:22 +0100

    feat: add custom input

--- components/Auth/Form/index.vue
+++ components/Auth/Form/index.vue
@@ -15,12 +15,12 @@
     <form class="space-y-8">
       <div v-if="type === 'sign-up'">
         <div class="flex gap-4">
-          <TextInput name="firstname" label="Prénom" placeholder="ex: John" />
-          <TextInput name="lastname" label="Nom" placeholder="ex: Doe" />
+          <CustomInput name="firstname" label="Prénom" placeholder="Entrez votre prénom" />
+          <CustomInput name="lastname" label="Nom" placeholder="Entrez votre nom" />
         </div>
       </div>
-      <EmailInput />
-      <PasswordSignInInput />
+      <CustomInput name="email" label="Email" placeholder="Entrez votre adresse électronique" />
+      <CustomInput name="password" label="Mot de passe" placeholder="Entrez votre mot de passe" />
       <div class="flex flex-col gap-4">
         <button type="submit" class="form-btn">
           {{ type === 'sign-in' ? 'Se connecter' : "S'inscrire" }}
---
+++ components/custom/input/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="form-item">
+    <label class="form-label">
+      {{ label }}
+    </label>
+    <div class="flex w-full flex-col">
+      <input :placeholder="placeholder" class="input-class" :type="name === 'password' ? 'password' : 'text'" />
+      <p v-if="errorMessage" class="form-message mt-2">{{ errorMessage }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    name: string;
+    label: string;
+    placeholder: string;
+    errorMessage?: string;
+  }>();
+</script>

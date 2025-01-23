85e9c7af017f0c89fa2206cb331198e6c75a3fa8
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-01-23 20:50:19 +0100

    tech: start to write first real unit test

---
+++ tests/authentication/sign-in.usecase.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from 'vitest';
+
+describe('As customer, I want to sign in, so I can enjoy application services', () => {
+  it('Given correct email and password When I sign in Then I can access to application services', async () => {
+    //  use spy user repository
+    //  assign sign in usecase
+    //  execute sign in usecase
+    //  test spy user repository args
+  });
+});

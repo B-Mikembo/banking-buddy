4e88bad4deb243458da5288c74f04d79875f740e
mxlcwm <malcom@gmail.com>
2025-02-10 21:13:12 +0100

    [FIX] default vue page overwrite layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <html lang="fr">
     <body>
       <main class="flex h-screen w-full font-inter">
-        <Sidebar v-if="useSupabaseSession().value !== null"/>
+        <Sidebar/>
         <div class="flex size-full flex-col">
           <div class="root-layout">
             <img src="/vite.svg" width="30" height="30" alt="logo" />

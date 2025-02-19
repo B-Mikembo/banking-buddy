dd9283250af14b1518a58f95ad2a50fd4932827b
B-Mikembo <mikembo.brice.pro@gmail.com>
2025-02-19 20:16:07 +0100

    chore(develop sign in tests): implement sign in test

--- tests/authentication/sign-in.spec.ts
+++ tests/authentication/sign-in.spec.ts
@@ -1,10 +1,15 @@
 import { describe, expect, it, test } from 'vitest';
 import { SpyUserRepository } from './adapters/user.repository.spy';
-
+import { SignInUsecase } from '~/domains/authentication/signIn.usecase';
 
 describe('Sign In Tests', () => {
-  it('should process sign in when I give email and password', async() => {
+  it('should process sign in when I give email and password', async () => {
     const spyUserRepository = new SpyUserRepository();
-    expect(false).toBe(true);
-  })
+    const usecase = new SignInUsecase(spyUserRepository);
+    await usecase.execute('john.doe@gmail.com', '123');
+    expect(spyUserRepository.signInUserArgs).toStrictEqual({
+      email: 'john.doe@gmail.com',
+      password: '123',
+    });
+  });
 });
